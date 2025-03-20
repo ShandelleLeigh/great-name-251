783bd39541c47dd66f9ebb7a2b7561d562ca8181
ShandelleLeigh <shandellejensen@gmail.com>
2025-03-20 17:23:00 -0600

    Update styles and content on intro

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,14 +1,15 @@
 <template lang="pug">
-div Portfolio
-	div
-		h2 Web Designs
+#portfolio
+	div Portfolio
+		div
+			h2 Web Designs
 
-		h3 Olympus Business Capitol Home Page
-		h3 Canine Revolution Dog Training Wordpress Home Page
-	div
-		h2 Illustrations
+			h3 Olympus Business Capitol Home Page
+			h3 Canine Revolution Dog Training Wordpress Home Page
+		div
+			h2 Illustrations
 
-		h3 End of Life Medical Decisions
+			h3 End of Life Medical Decisions
 </template>
 
 <script setup lang="ts">
--- src/components/intro.vue
+++ src/components/intro.vue
@@ -1,32 +1,12 @@
 <template lang="pug">
 #intro.grid.col
-	.row.col
+	.row.name
+		.col
 			h1.font-xl Hi, I'm Shandelle
 
-	.row.col
+	.row.about
+		.col
 			h2.font-3xl Web Developer
 			h3.font-2xl & UI/UX Designer
 			h4.font-xl based in Lehi
 </template>
-
-<script setup lang="ts">
-// import { ref } from "vue"
-
-// defineProps<{ msg: string }>()
-
-// // const count = ref(0)
-</script>
-
-<style lang="scss">
-.read-the-docs {
-	color: #888;
-}
-
-#intro{
-	gap: 5rem;
-	width: 100%;
-	border: 1px solid cyan;
-
-
-}
-</style>
